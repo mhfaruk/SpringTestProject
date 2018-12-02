c0824001085f00ddd017a8f4d8848a0cc45ed696
faruk <faruk@ufaruk>
2018-12-02 10:36:01 +0600

    add setters and getters and a constructor

--- SpringTest/src/main/java/com/test/main/model/TestModel.java
+++ SpringTest/src/main/java/com/test/main/model/TestModel.java
@@ -4,4 +4,28 @@ public class TestModel {
 
 	int id;
 	String name;
+	
+	
+	public TestModel() {
+		super();
+	}
+	public TestModel(int id, String name) {
+		super();
+		this.id = id;
+		this.name = name;
+	}
+	public int getId() {
+		return id;
+	}
+	public void setId(int id) {
+		this.id = id;
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	
+	
 }
